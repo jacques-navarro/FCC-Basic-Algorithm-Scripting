467c881a15da9b9bc817d3623a2905566cc8590a
Jacques Navarro <jacques.navarro@gmail.com>
2016-12-30 16:23:47 -0500

    Add files via upload

--- Reverse a String.js
+++ Reverse a String.js
@@ -1,16 +1,15 @@
 //https://www.freecodecamp.com/challenges/reverse-a-string#?solution=%0Afunction%20reverseString(str)%20%7B%0A%20%20return%20str%3B%0A%7D%0A%0AreverseString(%22hello%22)%3B%0A
 
-
 function reverseString(str) {
-  //convert str into an array separated at each character
-  var myArray = str.split("");
-  //reverse array elements
-  myArray.reverse();
-  //convert array elements into a string
-  //"" needed, commas added without quotes
-  str = myArray.join("");
 
-  return str;
+  //combined split, reverse and join array methods into one line
+  //split(""); convert str into an array separated at each character by ""
+  //reverse(); reverse array elements
+  //join(""); convert array elements into a string
+  //"" needed, commas added without quotes
+
+  return str.split("").reverse().join("");
+
 }
 
 reverseString("hello");
