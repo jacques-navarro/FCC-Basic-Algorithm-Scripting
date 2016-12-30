73ee311d2418516992b0666226abe313ac95d44f
Jacques Navarro <jacques.navarro@gmail.com>
2016-12-30 15:59:20 -0500

    Upload initial file state

---
+++ Reverse a String.js
@@ -0,0 +1,7 @@
+//https://www.freecodecamp.com/challenges/reverse-a-string#?solution=%0Afunction%20reverseString(str)%20%7B%0A%20%20return%20str%3B%0A%7D%0A%0AreverseString(%22hello%22)%3B%0A
+
+function reverseString(str) {
+  return str;
+}
+
+reverseString("hello");
