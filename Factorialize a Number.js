5ae4ab564d878a66de386f76631a71b78e2fc247
Jacques Navarro <jacques.navarro@gmail.com>
2016-12-30 16:36:21 -0500

    Initial state upload

---
+++ Factorialize a Number.js
@@ -0,0 +1,8 @@
+//Factorialize a Number
+//https://www.freecodecamp.com/challenges/factorialize-a-number#?solution=%0Afunction%20factorialize(num)%20%7B%0A%20%20return%20num%3B%0A%7D%0A%0Afactorialize(5)%3B%0A
+
+function factorialize(num) {
+  return num;
+}
+
+factorialize(5);
