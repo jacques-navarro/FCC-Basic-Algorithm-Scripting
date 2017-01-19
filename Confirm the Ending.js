645d0b8f8ae420f62c70a5ef1a1a7658804d8b9a
Jacques Navarro <jacques.navarro@gmail.com>
2017-01-19 03:07:43 -0500

    Initial state upload

---
+++ Confirm the Ending.js
@@ -0,0 +1,10 @@
+//Confirm the Ending
+//https://www.freecodecamp.com/challenges/confirm-the-ending
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  return str;
+}
+
+confirmEnding("Bastian", "n");
